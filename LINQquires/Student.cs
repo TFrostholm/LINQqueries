bdd55220f2abd1486e8587c571e272bae8fcab15
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-03-01 08:35:54 +0100

    Added a query to execute

---
+++ LINQquires/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LINQquires
+{
+    public class Student
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int ID { get; set; }
+        public List<int> Scores { get; set; } 
+    }
+}
